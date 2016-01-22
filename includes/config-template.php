15aa9a5f7a4f7134392c76b78a3c6f75989fad8f
Larry C. Parba <larz.parba@gmail.com>
2016-01-22 14:58:15 +0800

    Include files for VeteransRehab.com

---
+++ includes/config-template.php
@@ -0,0 +1,25 @@
+<?php
+    $domain =   "{{DOMAIN}}";
+    $domainid = "{{DOMAIN_ID}}";
+    $memberid = "{{MEMBER_ID}}";
+    $title = "{{TITLE}}";
+    $logo =  "{{LOGO}}";
+    $description = "{{DESCRIPTION}}";
+    $account_ga = "{{ACCOUNT_GA}}";
+    $description = stripslashes(str_replace('\n','<br>',$description));
+    $background_image = "{{BACKGROUND_IMAGE}}";
+    $fb_page =  "{{FB_PAGE}}";
+    $twitter = "{{TWITTER}}";
+    $bottom_text  = "{{BOTTOM_TEXT}}";
+    $forsale = "{{SHOW_FOR_SALE}}";
+    $forsaledefault = "{{SHOW_FOR_SALE_DEFAULT}}";
+	$forsaletext = "{{FOR_SALE_TEXT}}";
+    $footer_banner = "{{FOOTER_BANNER}}";
+    $domain_affiliate_link = "{{AFF_LINK}}";
+    $affiliate_id = "{{AFF_ID}}";
+    $additional_html = "{{ADDITIONAL_HTML}}";
+    $piwik_id = '{{PIWIK_ID}}';
+    $related_domains = {{RELATED_DOMAINS}};
+    $fund_campaigns = {{FUND_CAMPAIGNS}};
+    $partners = {{PARTNERS}};
+?>
